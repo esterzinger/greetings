10cc682c1a373491aaa9796304bfab07e5946462
esterzinger <esterrzinger@gmail.com>
2023-12-30 21:54:44 +0200

    update server

--- server/app.js
+++ server/app.js
@@ -1,27 +1,56 @@
 const express = require('express');
-const { OpenAIAPI } = require('openai');
-
+const process=require('dotenv').config();
 const app = express();
 const port = 3000;
 
-const openai = new OpenAIAPI({
-  key: 'YOUR_OPENAI_API_KEY',
-  model: 'text-davinci-002', // Adjust the model as needed
-});
+
+
+const apiKey = process.env.API_KEY;
+
 
 app.use(express.json());
 
-app.post('/generate', async (req, res) => {
+app.post('/generate-greeting', async (req, res) => {
   try {
-    const { prompt } = req.body;
+    const { event, age, type, atmosphere, NumberOfResponses = 1 } = req.body;
 
-    const gptResponse = await openai.complete({
-      prompt,
-      temperature: 0.7, // Adjust the temperature as needed
-    });
+    let prompt = `Please write me a greeting for `;
+    if (age) {
+      prompt += `${age} year old `;
+    }
+    switch (event) {
+      case 'birthday':
+        prompt += `for the birthday `
+        break;
+      case 'wedding':
+        prompt += `for the wedding `;
+        break;
+      default:
+        break;
+    }
+    if (type) prompt += `in a ${type} type `;
+    if (atmosphere) prompt += `with ${atmosphere} atmosphere `;
+    prompt += 'Please return 3 greetings in a parsable JSON format like follows: { "1": "first greeting", "2": "second greeting", "3": "third greeting" }'
 
-    const answer = gptResponse.data.choices[0].text.trim();
-    res.json({ answer });
+    const apiUrl = 'https://api.openai.com/v1/chat/completions';
+
+    const requestData = {
+      model: 'gpt-3.5-turbo',
+      messages: [
+        { role: 'user', content: prompt }
+      ],
+    };
+  
+    const generatedGreetings = fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify(requestData),
+    })
+    res.json({ greetings: generatedGreetings.choices[0].message.content });
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
--- server/openAI.js
+++ server/openAI.js
@@ -1,114 +1,29 @@
-//const { OpenAIApi } = require('openai');
 
-// const openai = new OpenAIApi({
-//   //api_key: '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"'
-// });
-// const {Configuration, OpenAIApi} =require('openai');
-// const config=new Configuration({
-//   apiKey: "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"
-// })
-//const openAI =new OpenAIApi(config)
+function generateAI(){
+  const apiUrl = 'https://api.openai.com/v1/chat/completions';
 
+  const requestData = {
+    model: 'gpt-3.5-turbo',
+    messages: [
+      { role: 'user', content: 'Who won the world series in 2020?' }
+    ],
+  };
 
-// import {OpenAI} from 'openai';
+  fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify(requestData),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.choices[0].message.content);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 
-// const openAI = new OpenAI({
-//   apiKey: "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", // This is the default and can be omitted
-// });
-//const OpenAI = require('openai');
-
-//const OpenAI =from "openai";
-
-//const openAI = new OpenAI({ apiKey: "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" });
-
-async function main() {
-  // const list = await openAI.fineTuning.jobs.list();
-
-  // for await (const fineTune of list) {
-  //   console.log(fineTune);
-  // }
-
-
-const r= await fetch("https://api.openai.com/v1/images/generations  ",{
-    method: 'POST', 
-    headers: {'Authorization': 'Bearer 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' ,
-    'Content-Type': 'application/json',},
-    body: JSON.stringify({
-      "model": "dall-e-3",
-      "prompt": "A cute baby sea otter",
-      "n": 1,
-      "size": "1024x1024"
-    }),
-    // Add other options like 'mode', 'credentials', etc. as needed
-  });
-  console.log(r)
 }
-main();
 
-
-// const openai = new OpenAIApi.OpenAI({ apiKey: "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" });
-// async function hh() {
-//   const response=await openai.embeddings.create({
-//     model: "text-embedding-ada-002",
-//     input: "The quick brown fox jumped over the lazy dog",
-//     encoding_format: "float",
-//   });
-
-// console.log(response)
-// }
-// hh()
-
-// openai.({
-//   model:"text-davinci-003",
-//   prompt:"Write me 3 a birthday greeting for a 4-year-old in a funny atmosphere in a short length",
-//   max_tokens:100,
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const completion = await openai.completions.create({
-//     model: 'gpt-3.5-turbo-instruct',
-//     prompt: 'Write a tagline for an ice cream shop.'
-// });
-//important link:)
-
-// https://platform.openai.com/docs/api-reference/completions/create
-
-// curl https://api.openai.com/v1/models \
-//   -H "Authorization: Bearer $OPENAI_API_KEY" \
-//   -H "OpenAI-Organization: org-uSbptIN7qjzeVX3k0W5tVJiP"
-// import fs from "fs";
-// import OpenAI from "openai";
-
-// const openai = new OpenAI({
-//   organization: 'org-uSbptIN7qjzeVX3k0W5tVJiP',
-// });
-
-// async function main() {
-//   const transcription = await openai.audio.transcriptions.create({
-//     file: fs.createReadStream("audio.mp3"),
-//     model: "whisper-1",
-//   });
-
-//   console.log(transcription.text);
-// }
-// main();
